99794ca23caf6150466e1f40013b9b4706cc1887
Jorge <jsanagui@gmail.com>
2024-07-14 17:14:47 +0200

    added update script

--- zsh/scripts.sh
+++ zsh/scripts.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+# Run script to update apt
+updatesys() {
+    sh $DOTFILES/update.sh
+}
+
+# Compress a file
 compress() {
-    tar cvzf $1.tar.gz $1
+    local DATE="$(date +%Y%m%d-%H%M%S)"
+    tar cvzf "$DATE.tar.gz" "$@"
 }
