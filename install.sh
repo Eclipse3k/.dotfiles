5b82e16a0dc26edbcd87d18c5e256d55f4603297
Jorge <jsanagui@gmail.com>
2024-07-15 01:22:01 +0200

    Tmux config added

--- install.sh
+++ install.sh
@@ -17,3 +17,26 @@ ln -sf "$DOTFILES/zsh/zshrc" "$ZDOTDIR/.zshrc"
 ln -sf "$DOTFILES/zsh/aliases" "$ZDOTDIR/aliases"
 rm -rf "$ZDOTDIR/external"
 ln -sf "$DOTFILES/zsh/external" "$ZDOTDIR"
+
+########
+# TMUX #
+########
+
+mkdir -p "$XDG_CONFIG_HOME/tmux"
+ln -sf "$DOTFILES/tmux/tmux.conf" "$HOME/.config/tmux"
+ln -sf "$DOTFILES/tmux/tmux.reset.conf" "$HOME/.config/tmux"
+rm -rf "$HOME/.config/tmux/scripts"
+ln -sf "$DOTFILES/tmux/scripts" "$HOME/.config/tmux"
+rm -rf "$HOME/.config/tmux/plugins"
+
+# Check if tpm is installed
+if [ ! -d "$TPM_PATH" ]; then
+    echo "Tmux Plugin Manager (tpm) not found, installing.. $HOME"
+    git clone https://github.com/tmux-plugins/tpm
+    mv ~/.tmux/plugins "$XDG_CONFIG_HOME/tmux/"
+    rm -rf ~/.tmux
+else
+    echo "Tmux Plugin Manager (tpm) is already installed."
+fi
+
+
