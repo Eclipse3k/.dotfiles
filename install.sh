017813b10389f435fe42ef666a977c4026b0d8c4
Jorge <jsanagui@gmail.com>
2024-07-14 16:41:41 +0200

    Add zsh config

--- install.sh
+++ install.sh
@@ -4,5 +4,16 @@
 # Neovim #
 ##########
 
-rm -rf "$HOME/.config/nvim"
-ln -s "$HOME/.dotfiles/nvim" "$HOME/.config"
+rm -rf "$XDG_CONFIG_HOME/nvim"
+ln -s "$DOTFILES/nvim" "$HOME/.config"
+
+#######
+# ZSH #
+#######
+
+mkdir -p "$ZDOTDIR"
+ln -sf "$DOTFILES/zsh/zshenv" "$HOME/.zshenv"
+ln -sf "$DOTFILES/zsh/zshrc" "$ZDOTDIR/.zshrc"
+ln -sf "$DOTFILES/zsh/aliases" "$ZDOTDIR/aliases"
+rm -rf "$ZDOTDIR/external"
+ln -sf "$DOTFILES/zsh/external" "$ZDOTDIR"
